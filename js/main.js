ba39948a01ff3124984408e5e8ac416086da64fa
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2023-05-08 17:37:51 +0200

    immagine

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ createApp({
             messaggio: `Crea il tuo nome `,
             nome: "",
             cognome: "",
-            cssNomeCognome: "nomeCognome"
+            cssNomeCognome: "nomeCognome",
+            immagine: ` src="./img/360_F_40451937_qem3GxnnaboDzPjgvZpudRXKx1lcRyk8.jpg"`
         }
     }
 }).mount(`#app`)
